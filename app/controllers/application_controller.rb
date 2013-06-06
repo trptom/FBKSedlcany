174125fb9371ff1b27aba94a3803e032df8fb738
trptom <prasltom@fel.cvut.cz>
2013-06-07 00:39:53 +0200

    Pridani stranky nekompletniho webu - pri produkci se zobrazuje vsude krome home/error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ include PermissionsHelper
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def redirect_if_production
+    if Rails.env.production? &&
+        (params[:controller] != "home" || params[:action] != "index")
+      redirect_to "/web_not_complete.html"
+    end
+  end
+
+  before_filter :redirect_if_production
+
 #  kdyz potrebuju resetovat prihlaseneho uzivatele
 #  def aaa
 #    login("admin", "root")
