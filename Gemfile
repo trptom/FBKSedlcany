9670287911e3eef5a607be4d51f4399bc9a785ac
Tomas Praslicak <tpraslicak@seznam.cz>
2013-08-14 12:58:14 +0200

    Uprava before_filteru u produkce tak, aby to povolovalo temer vse, vyjma aktualnich dostubnych stranek beynemu uzivateli, co jeste nejsou hotove (defacto jen info o tymu).

--- Gemfile
+++ Gemfile
@@ -14,6 +14,15 @@ gem 'cloudinary'
 
 #testovaci gemy
 gem 'simplecov', :require => false, :group => :test
+group :test do
+  gem 'selenium-webdriver'
+  gem 'test-unit'
+  gem 'capybara'
+end
+
+group :test, :development do
+  gem "rspec-rails", "~> 2.0"
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def redirect_if_production
-    if Rails.env.production? &&
-        (params[:controller] != "home" || params[:action] != "index")
+    if (Rails.env.production? || true) && (
+        (params[:controller] == "articles" && params[:action] == "search") ||
+        (params[:controller] == "teams" && params[:action] == "about") ||
+        (params[:controller] == "teams" && params[:action] == "squad") ||
+        (params[:controller] == "teams" && params[:action] == "matches")
+        (params[:controller] == "users" && params[:action] == "new"))
       redirect_to "/web_not_complete.html"
     end
   end
